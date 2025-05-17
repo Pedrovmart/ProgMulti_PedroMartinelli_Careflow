6572cbe82f4e71fec2b30d99a43a6c0991744f63
Pedro Vieira Martinelli <pedrovieiramartinelli@gmail.com>
2025-05-17 16:30:31 -0300

    refactor(profissionais): refatoração para busca de profissionais via n8n

--- lib/app/models/profissional_model.dart
+++ lib/app/models/profissional_model.dart
@@ -1,11 +1,9 @@
 import 'package:careflow_app/app/models/user_model.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Profissional extends UserModel {
   final String especialidade;
   final String numeroRegistro;
   final String idEmpresa;
-  final DateTime? createdAt;
   final String? dataNascimento;
   final String? telefone;
 
@@ -16,29 +14,20 @@ class Profissional extends UserModel {
     required this.especialidade,
     required this.numeroRegistro,
     required this.idEmpresa,
-    this.createdAt,
     this.dataNascimento,
     this.telefone,
   }) : super(userType: 'profissional');
 
   factory Profissional.fromJson(Map<String, dynamic> json) {
     return Profissional(
-      id: json['id'] ?? '', // Garante que 'id' nunca será null
-      nome: json['nome'] ?? 'Sem Nome', // Garante que 'nome' nunca será null
-      email:
-          json['email'] ?? 'Sem Email', // Garante que 'email' nunca será null
-      especialidade:
-          json['especialidade'] ?? 'Sem Especialidade', // Valor padrão
-      numeroRegistro:
-          json['numRegistro'] ??
-          'Sem Num. de Registro', // Corrigido para 'numRegistro'
-      idEmpresa: json['idEmpresa'] ?? '', // Valor padrão para 'idEmpresa'
-      createdAt:
-          json['createdAt'] != null
-              ? (json['createdAt'] as Timestamp).toDate()
-              : null, // Converte Timestamp para DateTime
-      dataNascimento: json['dataNascimento'], // Permite null
-      telefone: json['telefone'], // Permite null
+      id: json['id'] ?? '',
+      nome: json['nome'] ?? 'Sem Nome',
+      email: json['email'] ?? 'Sem Email',
+      especialidade: json['especialidade'] ?? 'Sem Especialidade',
+      numeroRegistro: json['numRegistro'] ?? 'Sem Num. de Registro',
+      idEmpresa: json['idEmpresa'] ?? '',
+      dataNascimento: json['dataNascimento'],
+      telefone: json['telefone'],
     );
   }
 
@@ -47,11 +36,10 @@ class Profissional extends UserModel {
     final json = super.toJson();
     json.addAll({
       'especialidade': especialidade,
-      'numRegistro': numeroRegistro, // Corrigido para 'numRegistro'
+      'numRegistro': numeroRegistro,
       'idEmpresa': idEmpresa,
-      'createdAt': createdAt, // Adicionado
-      'dataNascimento': dataNascimento, // Adicionado
-      'telefone': telefone, // Adicionado
+      'dataNascimento': dataNascimento,
+      'telefone': telefone,
     });
     return json;
   }
